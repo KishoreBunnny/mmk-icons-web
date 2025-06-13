456750352c2176f2fffd3025a251adf13b1bef81
KISHORE <kishore@KISHOREs-MacBook-Air.local>
2025-06-14 01:02:13 +0530

    next.config change

--- next.config.ts
+++ next.config.ts
@@ -1,12 +1,28 @@
-import type { NextConfig } from "next";
+// import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {
-  /* config options here */
+// const nextConfig: NextConfig = {
+//   /* config options here */
+//   output: 'export',
+//   reactStrictMode: true,
+// };
+
+// export default nextConfig;
+
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {
   output: 'export',
   reactStrictMode: true,
+  images: {
+    unoptimized: true, // Add this if you use next/image
+  },
 };
 
-export default nextConfig;
+module.exports = nextConfig;
+
+
+
+
 
 // /** @type {import('next').NextConfig} */
 // const nextConfig: import('next').NextConfig = {
