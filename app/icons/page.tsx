7517d9d072ffc3ec3f1bcf129d51f102e12f7ae8
KISHORE <kishore@KISHOREs-MacBook-Air.local>
2025-06-22 23:48:42 +0530

    fix used IconLoadingSkeleton

--- app/icons/page.tsx
+++ app/icons/page.tsx
@@ -3,7 +3,6 @@ import { Link } from "next-view-transitions";
 import { ReactNode } from "react";
 import { normalIcons } from "@/utils/normalcons"
 import { Metadata } from "next";
-import IconLoadingSkeleton from "@/components/IconLoadingSkeleton";
 
 export const metadata: Metadata = {
     title: "MMK Icons",
